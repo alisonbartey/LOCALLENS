9168ea694b0359b5040b895ec5a552ade7007196
alisonbartey <alisonbartey@gmail.com>
2025-11-02 11:04:42 -0800

    Fix token retrieval in API request interceptor

--- FlaskApiBackend/frontend/src/services/api.js
+++ FlaskApiBackend/frontend/src/services/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.setItem("token", response.data.access_token);
+  const token = localStorage.getItem("token"); // <-- get token
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
