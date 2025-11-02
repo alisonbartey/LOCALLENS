c1b3f9936ccf864d113b1a98efb770a2c0631b48
alisonbartey <alisonbartey@gmail.com>
2025-11-02 11:08:30 -0800

    Clarify token retrieval comment in api.js

--- FlaskApiBackend/frontend/src/services/api.js
+++ FlaskApiBackend/frontend/src/services/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token"); // <-- get token
+  const token = localStorage.getItem("token"); // GET token, don’t set it
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
